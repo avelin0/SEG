74407b0c46abb835073460af91140ea133371535
brunoavelino <avel.bruno@gmail.com>
2017-09-25 08:19:53 -0300

    C++ code cleaned

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -9,8 +9,6 @@
 
 extern "C"{
 
-
-
 using namespace std;
 using namespace cv;
 
@@ -54,8 +52,21 @@ void printMat(Mat mat, string str){
     }
 }
 
+/*! \brief color_watershed
+ *
+ *  Colore baseado no resultado do watershed. Como apos o watershed tem-se uma matriz na escala
+ *  de 0 a 255, caso os elementos vizinhos dessa matriz estejam com o mesmo valor, a cor sera a mesma,
+ *  caso não, colore com cores diferentes.
+ *
+ *  Obs: este algoritmo foi cedido pelo Thiago e Felipe, em seu trabalho de PFC no IME no ano de
+ *  2016.
+ *
+ * \param Mat imagem em formato da matriz a ser manipulada
+ * \param int comprimento da janela, valor default = 1
+ * \param string Identifica a imagem na janela
+ * \return Mat
+ */
 Mat color_watershed(Mat color) {
-    // Gera cores aleatorias
     vector<Vec3b> colors;
     for (size_t i = 0; i < 255; i++) {
         int b = theRNG().uniform(10, 255);
@@ -63,10 +74,8 @@ Mat color_watershed(Mat color) {
         int r = theRNG().uniform(10, 255);
         colors.push_back(Vec3b((uchar) b, (uchar) g, (uchar) r));
     }
-    // Cria imagem final
     Mat dst = Mat::zeros(color.size(), CV_8UC3);
 
-    // Pinta cada area de uma cor
     for (int i = 0; i < color.rows; i++) {
         for (int j = 0; j < color.cols; j++) {
 
@@ -78,16 +87,14 @@ Mat color_watershed(Mat color) {
                 dst.at<Vec3b>(i, j) = Vec3b(0, 0, 0);
         }
     }
-    printMat(dst,"destino");
     return dst;
 }
 
 Mat morpho_op(Mat src,int morph_elem = 0,int morph_size = 1,int morph_operator = 1,string mensagem="Morpho OP"){
     Mat dst;
-    int operation = morph_operator + 2;// Since MORPH_X : 2,3,4,5 and 6
+    int operation = morph_operator + 2;
     Mat element = getStructuringElement( morph_elem, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
 
-    /// Apply the specified morphology operation
     morphologyEx( src, dst, operation, element );
     return dst;
 }
@@ -140,10 +147,8 @@ bool cmpf(float a, float b, float epsilon = 0.005f) {
  */
 void step1(int x, int y) {
     if (val.at<float>(x, y) != 1) {
-        //inspecionar os vizinhos do pixel para setar o marcador
         for (int i =- windowSize; i <= windowSize; i++) {
             for (int j =- windowSize; j <= windowSize; j++) {
-                //Casos de borda
                 if (!(i == 0 && j == 0)) {
                     if (x + i >= 0 && y + j >= 0 && x + i <= img.rows - 1 && y + j <= img.cols - 1) {
 
@@ -172,8 +177,6 @@ void step2(int x, int y) {
             for (int j = -windowSize; j <= windowSize; j++) {
                 if (!(i == 0 && j == 0)) {
                     if (x + i >= 0 && y + j >= 0 && x + i <= img.rows - 1 && y + j <= img.cols - 1) {
-                        //min = maior vizinho
-                        //encontra o maior valor vizinho de minimo
                         if (img.at<float>(x, y) == img.at<float>(x + i, y + j) && val.at<float>(x + i, y + j) > 0 &&
                             val.at<float>(x + i, y + j) < min) {
                             min = val.at<float>(x + i, y + j);
@@ -182,7 +185,6 @@ void step2(int x, int y) {
                 }
             }
         }
-        //novo valor do pixel baseado nos minimos vizinhos
         if (min != VMAX && val.at<float>(x, y) != min + 1) {
             val.at<float>(x, y) = min + 1.0;
             if (min + 1.0 > LMAX) {
@@ -267,6 +269,9 @@ void step3(int x, int y) {
 /*! \brief Watersheed
  *
  *  Realiza a segmentação da imagem pelo algoritmo watersheed na variação rainfall
+ *  Obs: este algoritmo foi retirado de uma tese de mestrado - Implementation of Watershed Based
+ *  Image Segmentation Algorithm in FPGA, de Sameer Ruparelia.
+ *
  *
  * \param Mat imagem em formato da matriz a ser manipulada
  * \param int comprimento da janela, valor default = 1
@@ -283,38 +288,29 @@ JNIEXPORT void JNICALL Java_com_example_bruno_seg_GalleryActivity_watershed(
 
     cv::Mat& srcGray = *((cv::Mat*)scrGray);
     cv::Mat& matDst = *((cv::Mat*)dst);
-
-
-    img=srcGray.clone();
-    Mat bl;
-
-    bilateralFilter ( img, bl, 15, bilateralSigmaColorSpace, bilateralSigmaColorSpace);
-    img=bl.clone();
-
-
-    img=sobel(img,sobelThreshold);
-    img=morpho_op(img);
-
-
-
-
-
     new_label = 0.0;
     scan_step2 = 1;
     scan_step3 = 1;
     windowSize = 1;
-//    img = imagem;
+
+    img=srcGray.clone();
+
+    Mat bl;
+    bilateralFilter ( img, bl, 15, bilateralSigmaColorSpace, bilateralSigmaColorSpace);
+
+    img=bl.clone();
+
+    img=sobel(img,sobelThreshold);
+    img=morpho_op(img);
 
     img.convertTo(img, CV_32FC3);
 
     VMAX = 100000000000;
     LMAX = 100000000000;
 
-    //inicializar o tamanho da matriz
     lab = Mat::zeros(img.cols,img.rows,CV_32FC3);
     val = Mat::zeros(img.cols,img.rows,CV_32FC3);
 
-    //inicializar os valores
     for (int x = 0; x < img.rows; x++) {
         for (int y = 0; y < img.cols; y++) {
             lab.at<float>(x, y) = INIT;
@@ -323,14 +319,12 @@ JNIEXPORT void JNICALL Java_com_example_bruno_seg_GalleryActivity_watershed(
     }
 
 
-    //encontra menor pixel de cada vizinhanca
     for (int x = 0; x < img.rows; x++) {
         for (int y = 0; y < img.cols; y++) {
             step1(x, y);
         }
     }
 
-    //encontrar os plateaus mesmo pixel greyscale a partir dos minimos
     while (scan_step2 == 1) {
         changed = 0;
         //scan top left >> bottom right
@@ -360,7 +354,7 @@ JNIEXPORT void JNICALL Java_com_example_bruno_seg_GalleryActivity_watershed(
         limite++;
         if (limite == 10) break;
         changed = 0;
-        //scan top left >> bottom right
+        // top left >> bottom right
         for (int x = 0; x < img.rows; x++) {
             for (int y = 0; y < img.cols; y++) {
                 step3(x, y);
@@ -371,7 +365,7 @@ JNIEXPORT void JNICALL Java_com_example_bruno_seg_GalleryActivity_watershed(
             scan_step3 = 0;
         } else {
             changed = 0;
-            //scan bottom right >> top left
+            //bottom right >> top left
             for (int x = img.rows - 1; x >= 0; x--) {
                 for (int y = img.cols - 1; y >= 0; y--) {
                     step3(x, y);
@@ -495,19 +489,6 @@ JNIEXPORT void JNICALL Java_com_example_bruno_seg_CameraManip_morphoOp(JNIEnv *e
     /// Apply the specified morphology operation
     morphologyEx( mSrc, mDst, MORPH_CLOSE, element, Point(morph_size,morph_size), 3 );
 }
-
-//TODO:not working
-JNIEXPORT void JNICALL Java_com_example_bruno_seg_CameraManip_thresh(JNIEnv *env, jobject obj,
-                                                                     jlong scrGray,
-                                                                     jlong dst,
-                                                                     double thresholdValue=50.0,
-                                                                     double maxVal=100.0){
-    cv::Mat& matSrcGray = *((cv::Mat*)scrGray);
-    cv::Mat& matDst = *((cv::Mat*)dst);
-    threshold( matSrcGray, matDst, thresholdValue, maxVal, CV_THRESH_BINARY);
-}
-
-
 
 }
 
